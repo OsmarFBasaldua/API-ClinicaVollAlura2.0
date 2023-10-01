1fe785a2bb11be339367068c988847ae8806bff0
OsmarFBasaldua <osmar.f.basaldua@outlook.com>
2023-10-01 00:54:08 -0600

    Added Login & clave Error 403

---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.usuarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String calve;
+}
